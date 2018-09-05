84721cd2b678ff33486b27f8102d3f65b4be4934
root <root@rfledoc1.rfle.roto-frank.com>
2018-09-05 11:09:47 +0200

    moved configuration of folders from Dockerfile to run.sh

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,6 @@ MAINTAINER tacki@posteo.de
 RUN apk add --no-cache \
     bash apache2 apache2-webdav apache2-utils cifs-utils
 
-RUN mkdir -p /var/lib/dav \
-    && mkdir -p /run/apache2 \
-    && mkdir -p /var/webdav \
-    && chown apache:apache /var/lib/dav \
-    && chmod 755 /var/lib/dav
-
 ADD dav.conf /etc/apache2/conf.d/
 ADD run.sh /
 RUN chmod 750 /run.sh
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir -p /var/lib/dav \
+mkdir -p /run/apache2 \
+mkdir -p /var/webdav \
+chown apache:apache /var/lib/dav \
+chmod 755 /var/lib/dav
+ln -s /var/log/apache2/access.log /dev/stdout
+ln -s /var/log/apache2/error.log /dev/stderr
+
 htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:apache /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
